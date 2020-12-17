3556c6270da250bbdf14f2387823a31a47c8e279
Marcio Silveira <marcio.silveira@linx.com.br>
2020-12-17 17:45:31 -0300

    Remove test pages

--- ExchangeMachine/ClientApp/src/app/app.module.ts
+++ ExchangeMachine/ClientApp/src/app/app.module.ts
@@ -6,18 +6,12 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CashRegisterComponent } from './cash-register/cash-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     CashRegisterComponent
   ],
   imports: [
